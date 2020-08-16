4b2307552f07fc9d1d77baed542bd6008b930420
Sriramajeyam Sugumaran <yesoreyeram@gmail.com>
2020-08-16 16:43:11 +0100

    Hide limit control when using stats

--- src/servicenow/editor/OrderByCtrl.tsx
+++ src/servicenow/editor/OrderByCtrl.tsx
@@ -18,56 +18,52 @@ export class ServiceNowQueryOrderByCtrl extends PureComponent<any, any> {
     const orderByCtrl: any =
       query.servicenow.type === 'table' ? (
         <>
-          &nbsp;&nbsp;
-          <label className="gf-form-label width-8" title="Order By">
-            Order By
-          </label>
-          <Select
-            className="width-12"
-            value={
-              FIELDS_LIST.find((field: any) => field.value === query.servicenow.orderBy) || {
-                value: query.servicenow.orderBy,
-                label: query.servicenow.orderBy,
-              }
-            }
-            options={FIELDS_LIST.filter((field: any) => field.tables.indexOf(query.servicenow.table) > -1)}
-            defaultValue={query.servicenow.orderBy}
-            onChange={e => onSelectChange(e, `orderBy`, this.props)}
-            allowCustomValue
-          />
-          <Select
-            className="width-8"
-            value={ORDER_DIRECTIONS.find((service: any) => service.value === query.servicenow.orderByDirection)}
-            options={ORDER_DIRECTIONS}
-            defaultValue={query.servicenow.orderByDirection}
-            onChange={e => onSelectChange(e, 'orderByDirection', this.props)}
-          />
+          <div className="gf-form-inline">
+            <div className="gf-form">
+              <label className="gf-form-label width-8" title="Limit">
+                Limit
+              </label>
+              <Select
+                className="width-12"
+                value={
+                  LIMITS.find((service: any) => service.value === query.servicenow.limit) || {
+                    value: query.servicenow.limit,
+                    label: query.servicenow.limit,
+                  }
+                }
+                options={LIMITS}
+                defaultValue={query.servicenow.limit}
+                onChange={e => onSelectChange(e, 'limit', this.props, 'number')}
+                allowCustomValue
+              />
+              &nbsp;&nbsp;
+              <label className="gf-form-label width-8" title="Order By">
+                Order By
+              </label>
+              <Select
+                className="width-12"
+                value={
+                  FIELDS_LIST.find((field: any) => field.value === query.servicenow.orderBy) || {
+                    value: query.servicenow.orderBy,
+                    label: query.servicenow.orderBy,
+                  }
+                }
+                options={FIELDS_LIST.filter((field: any) => field.tables.indexOf(query.servicenow.table) > -1)}
+                defaultValue={query.servicenow.orderBy}
+                onChange={e => onSelectChange(e, `orderBy`, this.props)}
+                allowCustomValue
+              />
+              <Select
+                className="width-8"
+                value={ORDER_DIRECTIONS.find((service: any) => service.value === query.servicenow.orderByDirection)}
+                options={ORDER_DIRECTIONS}
+                defaultValue={query.servicenow.orderByDirection}
+                onChange={e => onSelectChange(e, 'orderByDirection', this.props)}
+              />
+            </div>
+          </div>
         </>
       ) : null;
-    return (
-      <div>
-        <div className="gf-form-inline">
-          <div className="gf-form">
-            <label className="gf-form-label width-8" title="Limit">
-              Limit
-            </label>
-            <Select
-              className="width-12"
-              value={
-                LIMITS.find((service: any) => service.value === query.servicenow.limit) || {
-                  value: query.servicenow.limit,
-                  label: query.servicenow.limit,
-                }
-              }
-              options={LIMITS}
-              defaultValue={query.servicenow.limit}
-              onChange={e => onSelectChange(e, 'limit', this.props, 'number')}
-              allowCustomValue
-            />
-            {orderByCtrl}
-          </div>
-        </div>
-      </div>
-    );
+    return <>{orderByCtrl}</>;
   }
 }
